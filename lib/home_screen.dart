6c873991d1a813a1c52ca24ca1d82559609134a0
mohemmad-alkatheri <77554202+mssk407@users.noreply.github.com>
2024-09-02 08:14:47 +0300

    add story section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,34 +1,39 @@
-import 'package:demo_camp/model/model.dart';
-import 'package:demo_camp/widget/app_drawer.dart';
-import 'package:demo_camp/widget/list_products.dart';
+import 'package:demo_camp/widget/bottom_bar_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  double d = 7;
-  List<Product> allProduct = [];
-  HomeScreen() {
-
-    allProduct.add(Product(name: "Iphone 13 proMax", price: 2000, descrbtion: "The best phone in the word!", imageURL: 'https://th.bing.com/th/id/R.540af3eace63949647d4202cfb56db5e?rik=ZBAAqkANz0fW9g&pid=ImgRaw&r=0' ,) );
-
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        drawer: AppDrawer(),
-        appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.camera_alt_outlined,
+          size: 40,
+          color: Colors.grey[600],
+        ),
+        titleSpacing: 3,
+        title: Text(
+          "Instagram",
+          style: TextStyle(
+              decoration: TextDecoration.none,
+              fontFamily: "Billabong",
+              fontSize: 30),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 35,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon:Image.asset("assets/images/masenger.png",width: 60,)),
+        ],
 
-          title: const Text("Home Screen 3"),
-          backgroundColor: const Color.fromARGB(255, 82, 25, 155),
-        ),
-        body: Column(
-            children:allProduct.map((x)=>buildListProducts(context,x)).toList()
-        ),
       ),
+      bottomNavigationBar: BottomBarWidget(),
+body: Image(image: AssetImage("assets/images/reels.png"),width: 20,),
     );
   }
-
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:demo_camp/home_screen.dart';
+ 
 import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +10,9 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
-        home: HomeScreen());
+      home:HomeScreen());
   }
 
 }
+
+
